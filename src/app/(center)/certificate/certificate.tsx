f75e341c0a1a60e4300eae03378449cf9f961316
kuldeepbagga <kuldeep9713@gmail.com>
2025-08-29 13:20:15 +0530

    CHANGES IMP

--- src/app/(center)/certificate/certificate.tsx
+++ src/app/(center)/certificate/certificate.tsx
@@ -1,9 +1,14 @@
 import axiosClient from "@/axios";
 import ProtectedRoute from "@/src/component/ProtectedRoute";
+import { useAuth } from "@/src/context/AuthContext";
+import { Buffer } from "buffer";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
 import { useRouter } from "expo-router";
 import React, { useCallback, useEffect, useState } from "react";
 import {
   ActivityIndicator,
+  Alert,
   FlatList,
   StyleSheet,
   Text,
@@ -12,8 +17,10 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+global.Buffer = global.Buffer || Buffer;
+
 const CertificateCard = React.memo(
-  ({ certificate, onViewMarksheet, onViewDiploma }) => (
+  ({ certificate, onViewMarksheet, downloadDocument }) => (
     <View style={styles.card}>
       <View style={styles.info}>
         <Text style={styles.label}>
@@ -32,15 +39,32 @@ const CertificateCard = React.memo(
 
       <View style={styles.actions}>
         <TouchableOpacity
-          onPress={() => onViewMarksheet(certificate)}
-          style={styles.editBtn}>
+          onPress={() =>
+            onViewMarksheet(
+              certificate.enrollment_no,
+              certificate.certificate_no
+            )
+          }
+          style={styles.editBtn}
+        >
           <Text style={styles.btnText}>View Marksheet</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          onPress={() => onViewDiploma(certificate)}
-          style={styles.deleteBtn}>
-          <Text style={styles.btnText}>View Diploma</Text>
+          onPress={() =>
+            downloadDocument(
+              certificate.enrollment_no,
+              certificate.certificate_no,
+              certificate.certificate_type
+            )
+          }
+          style={styles.deleteBtn}
+        >
+          <Text style={[styles.btnText]}>
+            Download{" "}
+            {certificate.certificate_type.charAt(0).toUpperCase() +
+              certificate.certificate_type.slice(1)}
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -50,14 +74,14 @@ const CertificateCard = React.memo(
 const certificate = () => {
   const PAGE_SIZE = 100;
   const router = useRouter();
+  const { user, logout } = useAuth();
   const [certificates, setCertificates] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDownloading, setIsDownloading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState(1);
 
   const getCertificateData = async () => {
-    setIsLoading(true);
-
     try {
       const res = await axiosClient.get(
         `certificate/index?page=${page}&limit=${PAGE_SIZE}`
@@ -77,13 +101,86 @@ const certificate = () => {
   }, []);
 
   const renderItem = useCallback(
-    ({ item }) => (
+    ({ item, isloading }) => (
       <CertificateCard
         certificate={item}
-        onViewMarksheet={() => alert("hello")}
-        onViewDiploma={() => alert(item.id)}
+        downloadMarksheet={() =>
+          handleDownloadMarksheet(item.certificate_no, item.enrollment_no)
+        }
+        downloadDocument={() =>
+          handleDocumentDownload(
+            item.certificate_no,
+            item.enrollment_no,
+            item.certificate_type
+          )
+        }
       />
     ),
+    []
+  );
+
+  const handleDownloadMarksheet = useCallback(
+    async (certificate_no, enrollment_no) => {},
+    []
+  );
+
+  const handleDocumentDownload = useCallback(
+    async (certificate_no, enrollment_no, type) => {
+      setIsDownloading(true);
+
+      let URLPATH = "";
+
+      if (type === "diploma") {
+        URLPATH = "diploma/download";
+      } else if (type === "certificate") {
+        URLPATH = "certificate/download";
+      }
+
+      if (URLPATH === "") return Alert.alert("something went wrong!");
+
+      try {
+        const res = await axiosClient.post(
+          URLPATH,
+          {
+            certificateNo: certificate_no,
+            enrollmentNo: enrollment_no,
+          },
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              "Content-Disposition": "attachment;filename=download.jpg",
+            },
+            responseType: "arraybuffer",
+          }
+        );
+
+        // 3. Convert binary to base64
+        const base64 = Buffer.from(res.data, "binary").toString("base64");
+
+        // 4. Save base64 image temporarily
+        const fileUri = FileSystem.cacheDirectory + "diploma.jpg";
+        await FileSystem.writeAsStringAsync(fileUri, base64, {
+          encoding: FileSystem.EncodingType.Base64,
+        });
+
+        // 5. Save the file to the device's Gallery
+        const asset = await MediaLibrary.createAssetAsync(fileUri);
+        await MediaLibrary.createAlbumAsync("Diplomas", asset, false);
+        Alert.alert("Success", "Image saved to gallery!");
+      } catch (err) {
+        console.log(err);
+        if (err.response.status === 401) {
+          await logout();
+          router.push("/");
+        }
+        // if (err.response.status === 422) {
+        //   Alert.alert(err.response.message);
+        // }
+        Alert.alert(err.response.data.message);
+      } finally {
+        setIsDownloading(false);
+      }
+    },
     []
   );
 
@@ -98,14 +195,17 @@ const certificate = () => {
 
   if (certificates.length <= 0 && !isLoading) {
     return (
-      <Text
-        style={{
-          textAlign: "center",
-          marginTop: 20,
-          fontWeight: "bold",
-        }}>
-        No record found!
-      </Text>
+      <View style={{ paddingVertical: 16 }}>
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: 20,
+            fontWeight: "bold",
+          }}
+        >
+          No record found!
+        </Text>
+      </View>
     );
   }
 
@@ -113,7 +213,17 @@ const certificate = () => {
     <ProtectedRoute>
       <SafeAreaView
         edges={["left", "right", "bottom"]}
-        style={{ flex: 1, backgroundColor: "#f8f8f8" }}>
+        style={{ flex: 1, backgroundColor: "#f8f8f8" }}
+      >
+        {isDownloading && (
+          <View style={styles.loadingOverlay}>
+            <View style={styles.loaderBox}>
+              <ActivityIndicator size="large" color="#fff" />
+              <Text style={styles.loaderText}>Please Wait...</Text>
+            </View>
+          </View>
+        )}
+
         <View style={{ flex: 1 }}>
           <FlatList
             data={certificates}
@@ -212,5 +322,28 @@ const styles = StyleSheet.create({
   btnText: {
     color: "#fff",
     fontWeight: "bold",
+  },
+
+  loadingOverlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 999,
+  },
+  loaderBox: {
+    backgroundColor: "rgba(0,0,0,0.8)",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  loaderText: {
+    marginTop: 10,
+    color: "#fff",
+    fontSize: 16,
   },
 });
